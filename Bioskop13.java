d62326ed4aa6aa6be517631995beb91b5839436a
kmilaz14 <kamilazhwa@gmail.com>
2024-11-07 09:39:26 +0800

    pertanyaan 7

--- Bioskop13.java
+++ Bioskop13.java
@@ -21,6 +21,12 @@ public class Bioskop13 {
         for (String[] barisPenonton : penonton){
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
+
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (int i = 0; i<penonton[2].length; i++){
+            System.out.println(penonton[2][i]);
+        }
         
     }
 }
