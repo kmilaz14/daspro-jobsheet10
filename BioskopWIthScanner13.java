f6f13a2ef9614783ee08fa4d6a009b2da59475b8
kmilaz14 <kamilazhwa@gmail.com>
2024-11-07 11:27:04 +0800

    pertanyaan 5

--- BioskopWIthScanner13.java
+++ BioskopWIthScanner13.java
@@ -10,7 +10,7 @@ public interface BioskopWIthScanner13 {
         String[][] penonton = new String[4][2];
 
         do{
-            System.out.println("-----Menu--------");
+            System.out.println("--------Menu--------");
             System.out.println("1. Input data penonton");
             System.out.println("2. Tampilkan daftar penonton");
             System.out.println("3. Exit");
@@ -22,23 +22,27 @@ public interface BioskopWIthScanner13 {
                 case 1:
                     System.out.print("Masukkan nama: ");
                     nama = sc.nextLine();
-                    System.out.print("Masukkan baris (1-4): ");
-                    baris = sc.nextInt();
-                    System.out.print("Masukkan kolom (1-2): ");
-                    kolom = sc.nextInt();
-                    sc.nextLine();
+                    boolean kursiTersedia = false;
+                    while (!kursiTersedia) {
+                        System.out.print("Masukkan baris (1-4): ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan kolom (1-2): ");
+                        kolom = sc.nextInt();
+                        sc.nextLine();
 
-                    if(baris>= 1 && baris <=4 && kolom>=1 && kolom <=2){
-                        if (penonton[baris-1][kolom-1] == null) {
-                            System.out.println("Baris dan kolom tersedia.");
-                            penonton[baris-1][kolom-1] = nama;
+                        if(baris>= 1 && baris <=4 && kolom>=1 && kolom <=2){
+                            if (penonton[baris-1][kolom-1] == null) {
+                                penonton[baris-1][kolom-1] = nama;
+                                System.out.println("Kursi baris " + baris + ", kolom " + kolom + " berhasil dipesan.");
+                                kursiTersedia = true;
+                            } else {
+                                System.out.println("Kursi sudah terisi. Silahkan pilih kursi lain!");
+                            }
                         } else {
-                            System.out.println("Kursi sudah terisi. Silahkan pilih kursi lain!");
+                            System.out.println("Nomor baris/kolom tidak tersedia.");
                         }
-                    } else {
-                        System.out.println("Nomor baris/kolom tidak tersedia.");
-                    }
-                    break;
+                    } break;
+                   
                 case 2:
                     System.out.println("Daftar Penonton: ");
                     boolean adaPenonton = false;
@@ -52,7 +56,18 @@ public interface BioskopWIthScanner13 {
                     } if(!adaPenonton){
                         System.out.println("Belum ada penonton yang terdaftar.");
                     }
-                    break; 
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            if (penonton[i][j] == null) {
+                                System.out.print("***\t");  
+                            } else {
+                                System.out.print(penonton[i][j] + "\t");  
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
+
                 case 3:
                     System.out.println("Keluar dari program.");
                     break;
