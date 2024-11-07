2ec8736b5d2a438cf1a42b0273913ecde4d96cf4
kmilaz14 <kamilazhwa@gmail.com>
2024-11-07 10:59:25 +0800

    pertanyaan 2

--- BioskopWIthScanner13.java
+++ BioskopWIthScanner13.java
@@ -4,28 +4,56 @@ public interface BioskopWIthScanner13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        int baris, kolom;
-        String nama, next;
+        int baris = 0, kolom = 0, pilihan;
+        String nama = "", next;
 
         String[][] penonton = new String[4][2];
 
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+        do{
+            System.out.println("-----Menu--------");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris (1-4): ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom (1-2): ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                    if(baris>= 1 && baris <=4 && kolom>=1 && kolom <=2){
+                        penonton[baris-1][kolom-1] = nama;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Daftar Penonton: ");
+                    boolean adaPenonton = false;
+                    for (int i=0; i<4; i++){
+                        for(int j=0; j<2; j++){
+                            if (penonton[i][j] != null) {
+                                adaPenonton = true;
+                                System.out.println("Baris: " + (i+1) + ", Kolom " + (j+1) + ": " + penonton[i][j]);
+                            }
+                        }
+                    } if(!adaPenonton){
+                        System.out.println("Belum ada penonton yang terdaftar.");
+                    }
+                    break; 
+                case 3:
+                    System.out.println("Keluar dari program.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silahkan pilih 1, 2, atau 3!");
+                    break;
+            } 
+        } while (pilihan != 3);
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
-            }
-        }
     }
 }
